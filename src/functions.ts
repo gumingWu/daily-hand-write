6d16886a859b41b9bbe5ae211981e51e0d9f442b
gumingWu <15626185054@163.com>
2022-07-06 00:08:09 +0800

    创建md方法

--- src/functions.ts
+++ src/functions.ts
@@ -2,6 +2,7 @@ import fs from 'fs-extra'
 import { join, resolve } from 'path'
 
 const DIR_PACKAGE = resolve(__dirname, '..')
+const DIR_CREATED_MD_PACKAGE = resolve(__dirname, '../autoPackage')
 
 export let Issues_Total = 0
 export let Issues_Content = null
@@ -10,7 +11,25 @@ export async function resolveIssuesList(issuesList) {
   Issues_Total = issuesList.length
   Issues_Content = issuesList
 
-  await fs.writeJson(join(DIR_PACKAGE, 'index.json'), issuesList, { spaces: 2 })
+  // 创建json文件，方便看数据
+  // await fs.writeJson(join(DIR_PACKAGE, 'index.json'), issuesList, { spaces: 2 })
+
+  // 遍历issues，创建md文件
+  await fs.removeSync(DIR_CREATED_MD_PACKAGE)
+  await fs.ensureDirSync(DIR_CREATED_MD_PACKAGE)
+  await Promise.all(issuesList.map(issuesItem => createIssuesMd(issuesItem)))
+}
+
+export async function createIssuesMd(issuesItem) {
+  const { title, created_at, body } = issuesItem
+
+  // 裁剪创建时间年月为目录
+  const yearMon = created_at.slice(0, 7)
+  const mdDirPath = join(DIR_CREATED_MD_PACKAGE, yearMon)
+  await fs.ensureDirSync(mdDirPath)
+
+  const mdPath = join(mdDirPath, `${title.replace(/:|：/, '.')}.md`)
+  await fs.createFileSync(mdPath)
 }
 
 export function getMainSidebar() {
