57dca4ff51fb95e74ab91ec90e2624f122ce777e
gumingWu <15626185054@163.com>
2022-07-09 01:18:56 +0800

    优化信息组件

---
+++ .vitepress/components/IssueInfo.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+
+const props = defineProps({
+  createdAt: String,
+  updatedAt: String,
+  creator: String
+})
+
+const formatCteatedAt = dayjs(props.createdAt).format('YYYY-MM-DD HH:mm:ss')
+const formatUpdatedAt = dayjs(props.updatedAt).format('YYYY-MM-DD HH:mm:ss')
+</script>
+
+<template>
+  <div class="issue-info-wrap">
+    <div class="item">
+      <label>Created at</label>
+      <div>{{ formatCteatedAt }}</div>
+    </div>
+    <div class="item">
+      <label>Updated at</label>
+      <div>{{ formatUpdatedAt }}</div>
+    </div>
+    <div class="item">
+      <label>Creator</label>
+      <div>{{ creator }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.issue-info-wrap {
+  display: flex;
+  flex-direction: column;
+  margin: 15px 0;
+  font-size: 14px;
+  border-radius: 4px;
+  box-shadow: -2px -2px 5px #fff, 2px 2px 5px #babecc;
+  transition: all .3s;
+}
+.issue-info-wrap:hover {
+  box-shadow: -5px -5px 20px #fff, 5px 5px 20px #babecc;
+}
+
+.item {
+  display: flex;
+}
+.item label {
+  opacity: 50%;
+  width: 150px;
+}
+</style>
---
+++ docs/assets/autoPackage_2022-06_34.实现 LazyMan.md.f015f1a9.js
@@ -0,0 +1,33 @@
+import{_ as n}from"./chunks/IssueInfo.e9cf9a9a.js";import{_ as l,c as p,d as o,a as s,b as e,e as t,o as c}from"./app.efce2d2a.js";const h=JSON.parse('{"title":"34.\u5B9E\u73B0 LazyMan","description":"","frontmatter":{},"headers":[],"relativePath":"autoPackage/2022-06/34.\u5B9E\u73B0 LazyMan.md"}'),r={name:"autoPackage/2022-06/34.\u5B9E\u73B0 LazyMan.md"},y=s("h1",{id:"_34-\u5B9E\u73B0-lazyman",tabindex:"-1"},[e("34.\u5B9E\u73B0 LazyMan "),s("a",{class:"header-anchor",href:"#_34-\u5B9E\u73B0-lazyman","aria-hidden":"true"},"#")],-1),i=t(`<div class="language-js"><span class="copy"></span><pre><code><span class="line"><span style="color:#82AAFF;">LazyMan</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">Hank</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// \u8F93\u51FA:</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Hi! This is Hank!</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">LazyMan</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">Hank</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">sleep</span><span style="color:#A6ACCD;">(</span><span style="color:#F78C6C;">3</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">eat</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">dinner</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// \u8F93\u51FA:</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Hi! This is Hank!</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// //\u7B49\u5F853\u79D2..</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Wake up after 3</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Eat dinner~</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">LazyMan</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">Hank</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">eat</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">dinner</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">eat</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">supper</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// \u8F93\u51FA:</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Hi This is Hank!</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Eat dinner~</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Eat supper~</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">LazyMan</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">Hank</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">sleepFirst</span><span style="color:#A6ACCD;">(</span><span style="color:#F78C6C;">2</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">eat</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">dinner</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">sleep</span><span style="color:#A6ACCD;">(</span><span style="color:#F78C6C;">3</span><span style="color:#A6ACCD;">)</span><span style="color:#89DDFF;">.</span><span style="color:#82AAFF;">eat</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">supper</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// \u8F93\u51FA:</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// //\u7B49\u5F852\u79D2..</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Wake up after 2</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Hi This is Hank!</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Eat dinner~</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// //\u7B49\u5F853\u79D2..</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Wake up after 2</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// Eat supper~</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">// \u4EE5\u6B64\u7C7B\u63A8</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u590D\u5236\u4EE3\u7801</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span></code></pre></div>`,1);function D(F,A,C,_,E,d){const a=n;return c(),p("div",null,[y,o(a,{"created-at":"2022-06-08T15:23:09Z","updated-at":"2022-07-08T08:54:40Z",creator:"Baozhen-Yin"}),i])}var k=l(r,[["render",D]]);export{h as __pageData,k as default};
