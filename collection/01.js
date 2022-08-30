dd787e91576282922bf2659affa8da2fde54dc39
gumingWu <15626185054@163.com>
2022-08-30 16:29:30 +0800

    实现groupToMap方法

--- collection/01.js
+++ collection/01.js
@@ -24,3 +24,32 @@ const res = array.myGroup((num, index, array) => {
   return num % 3 === 0 ? 'aa': 'bb'
 })
 console.log(res);
+
+Array.prototype.myGroupToMap = function(callback, thisArg=null) {
+  if(typeof callback !== 'function') {
+    throw new TypeError('not a function')
+  }
+
+  const arr = this
+  const length = arr.length
+  const grouper = new Map()
+
+  for(let i=0; i<length; i++) {
+    const key = callback.call(thisArg, arr[i], i, arr)
+    if(!grouper.has(key)) {
+      grouper.set(key, [arr[i]])
+    } else {
+      grouper.get(key).push(arr[i])
+    }
+  }
+
+  return grouper
+}
+
+const odd  = { odd: true };
+const even = { even: true };
+const array2 = [1, 2, 3, 4, 5]
+const map = array2.myGroupToMap((num, index, array) => {
+  return num % 2 === 0 ? even: odd;
+});
+console.log(map);
